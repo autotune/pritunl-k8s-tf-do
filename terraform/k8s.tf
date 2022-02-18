1f508ffe2b2bf1c52febb2b2ca19d1e381990b2d
Brian Adams <service@contrasting.org>
2022-02-18 17:42:08 -0600

    fix: hardcode k8s version

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -3,20 +3,11 @@ resource "random_id" "cluster_name" {
   byte_length = 6
 }
 
-data "external" "get_latest_do_k8s_version" {
-  count   = var.enable_digitalocean ? 1 : 0
-  program = ["sh", "${path.module}/get_do_latest_k8s_version.sh"]
-
-  query = {
-    do_token = var.do_token
-  }
-}
-
 resource "digitalocean_kubernetes_cluster" "k8s" {
   count   = var.enable_digitalocean ? 1 : 0
   name    = "${var.do_k8s_name}-${random_id.cluster_name[count.index].hex}"
   region  = var.do_region
-  version = data.external.get_latest_do_k8s_version[count.index].result["version"]
+  version = 1.20.2-do.0 
 
   node_pool {
     name       = var.do_k8s_pool_name
