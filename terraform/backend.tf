f3456388a2180604f3c08e9b0e95bf77c2bee5db
Brian Adams <service@contrasting.org>
2022-02-18 19:08:15 -0600

    fix: remove unsupported ec2 backend args

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,9 +4,7 @@ terraform {
     key                         = "pritunl.tfstate"
     bucket                      = "badams"
     region                      = "us-east-1"
-    skip_requesting_account_id  = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms      = true
     skip_metadata_api_check     = true
   }
 }
