0a8daada2b22b3e082e9840d007dd795571aa23b
Brian Adams <service@contrasting.org>
2022-02-18 19:41:30 -0600

    feat: add k8s provider

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -32,6 +32,7 @@ resource "local_file" "kubeconfigdo" {
 }
 
 data "digitalocean_kubernetes_cluster" "k8s" {
+  count   = var.enable_digitalocean ? 1 : 0
   name = digitalocean_kubernetes_cluster.k8s[count.index].id 
 }
 
