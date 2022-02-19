72ab008cf4500013691fbc81162b73aeb8a83eb1
Brian Adams <service@contrasting.org>
2022-02-18 19:37:05 -0600

    feat: add k8s provider

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -30,3 +30,15 @@ resource "local_file" "kubeconfigdo" {
   content  = digitalocean_kubernetes_cluster.k8s[count.index].kube_config[0].raw_config
   filename = "${path.module}/kubeconfig_do"
 }
+
+data "digitalocean_kubernetes_cluster" "k8s" {
+  name = digitalocean_kubernetes_cluster.k8s.id
+}
+
+provider "kubernetes" {
+  host             = data.digitalocean_kubernetes_cluster.k8s.endpoint
+  token            = data.digitalocean_kubernetes_cluster.k8s.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
+  )
+}
