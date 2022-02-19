adf8784734a588f8ac70916a7a4b377e9e0e1292
Brian Adams <service@contrasting.org>
2022-02-18 22:56:23 -0600

    feat: add atlantis to dns

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -14,3 +14,15 @@ resource "digitalocean_record" "a_records" {
     kubernetes_ingress.default_cluster_ingress
   ]
 }
+
+resource "digitalocean_record" "atlantis" {
+  for_each = toset(var.domain_name)
+  domain = "atlantis.${each.value}"
+  type   = "A"
+  ttl = 60
+  name   = "@"
+  value  = digitalocean_loadbalancer.ingress_load_balancer.ip
+  depends_on = [
+    kubernetes_ingress.default_cluster_ingress
+  ]
+}
