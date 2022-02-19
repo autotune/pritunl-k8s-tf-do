8045df38c36d4b391de1d54b22f36af7e3ee5684
Brian Adams <service@contrasting.org>
2022-02-19 13:58:34 -0600

    feat: add atlantis lb

--- terraform/cluster_resources.tf
+++ terraform/cluster_resources.tf
@@ -71,3 +71,78 @@ resource "kubernetes_service" "atlantis" {
     }
   }
 }
+
+
+resource "kubernetes_deployment" "atlantis_deployments" {
+  for_each = toset(var.domain_name)
+  metadata {
+    name = "${replace(each.value, ".", "-")}-atlantis-deployment"
+    namespace="default"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "${replace(each.value, ".", "-")}-atlantis-deployment"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app  = "${replace(each.value, ".", "-")}-atlantis-deployment"
+        }
+      }
+      spec {
+        container {
+          image = "nginxdemos/hello"
+          name  = "nginx-hello"
+          port {
+            container_port = 80
+          }
+          resources {
+            limits = {
+              memory = "512M"
+              cpu = "1"
+            }
+            requests = {
+              memory = "256M"
+              cpu = "50m"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "atlantis_services" {
+  for_each = toset(var.domain_name)
+  metadata {
+    name      = "${replace(each.value, ".", "-")}-atlantis-service"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "${replace(each.value, ".", "-")}-atlantis-deployment"
+    }
+    port {
+      port = 80
+    }
+  }
+}
+
+resource "kubernetes_service" "atlantis" {
+  for_each = toset(var.domain_name)
+  metadata {
+    name      = "${replace(each.value, ".", "-")}-atlantis-service"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "${replace(each.value, ".", "-")}-atlantis-deployment"
+    }
+    port {
+      port = 80
+    }
+  }
+}
