f1fcbcaf524e89009f0ec5c39ac67b518fbe0b37
Brian Adams <service@contrasting.org>
2022-02-18 22:48:36 -0600

    feat: added cluster resources

---
+++ terraform/cluster_resources.tf
@@ -0,0 +1,57 @@
+resource "kubernetes_deployment" "sample_deployments" {
+  for_each = toset(var.domain_name)
+  metadata {
+    name = "${replace(each.value, ".", "-")}-deployment"
+    namespace="default"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "${replace(each.value, ".", "-")}-deployment"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app  = "${replace(each.value, ".", "-")}-deployment"
+        }
+      }
+      spec {
+        container {
+          image = "nginxdemos/hello"
+          name  = "nginx-hello"
+          port {
+            container_port = 80
+          }
+          resources {
+            limits = {
+              memory = "512M"
+              cpu = "1"
+            }
+            requests = {
+              memory = "256M"
+              cpu = "50m"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "sample_services" {
+  for_each = toset(var.domain_name)
+  metadata {
+    name      = "${replace(each.value, ".", "-")}-service"
+    namespace = "default"
+  }
+  spec {
+    selector = {
+      app = "${replace(each.value, ".", "-")}-deployment"
+    }
+    port {
+      port = 80
+    }
+  }
+}
