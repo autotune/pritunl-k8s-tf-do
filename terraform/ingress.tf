9d3efa6f63e402d545697c57e8902250e1821430
Brian Adams <service@contrasting.org>
2022-02-19 14:24:40 -0600

    fix: set atlantis in separate ns

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -16,24 +16,6 @@ resource "helm_release" "nginx_ingress_chart" {
   ]
 }
 
-resource "helm_release" "atlantis_nginx_ingress_chart" {
-  name       = "nginx-ingress-controller"
-  namespace  = "atlantis"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-  set {
-    name  = "service.type"
-    value = "LoadBalancer"
-  }
-  set {
-    name  = "service.annotations.kubernetes\\.digitalocean\\.com/load-balancer-id"
-    value = digitalocean_loadbalancer.atlantis_ingress_load_balancer.id
-  }
-  depends_on = [
-    digitalocean_loadbalancer.ingress_load_balancer,
-  ]
-}
-
 resource "kubernetes_ingress" "default_cluster_ingress" {
   depends_on = [
     helm_release.nginx_ingress_chart,
