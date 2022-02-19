7711edbfa53eea3e57c5712c52408e2ee4149dfa
Brian Adams <service@contrasting.org>
2022-02-18 20:54:37 -0600

    feat: create a record to lb

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,3 +1,15 @@
 data "digitalocean_domain" "default" {
   name = var.domain_name
 }
+
+resource "digitalocean_record" "a_records" {
+  domain = var.domain_name 
+  type   = "A"
+  ttl = 60
+  name   = "@"
+  value  = digitalocean_loadbalancer.ingress_load_balancer.ip
+  depends_on = [
+    digitalocean_domain.top_level_domains,
+    kubernetes_ingress.default_cluster_ingress
+  ]
+}
