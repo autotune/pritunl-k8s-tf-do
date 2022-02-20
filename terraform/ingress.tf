5134b114c6737fbb28224fd52e2bd5fe8c039885
Brian Adams <service@contrasting.org>
2022-02-20 15:10:26 -0600

    fix: lb

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -7,12 +7,10 @@ resource "helm_release" "nginx_ingress_chart" {
     name  = "service.type"
     value = "LoadBalancer"
   }
-  /* 
   set {
     name  = "service.annotations.kubernetes\\.digitalocean\\.com/load-balancer-id"
     value = digitalocean_loadbalancer.ingress_load_balancer.id
   }
-  */ 
   depends_on = [
     digitalocean_loadbalancer.ingress_load_balancer,
   ]
