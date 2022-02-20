180d604d934e384e60a3ac828f83468ff008f2d7
Brian Adams <service@contrasting.org>
2022-02-20 17:54:39 -0600

    fix: set voc uuid on lb

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -19,27 +19,5 @@ resource "digitalocean_loadbalancer" "ingress_load_balancer" {
     ]
   }
 
-}
-
-resource "digitalocean_loadbalancer" "atlantis_ingress_load_balancer" {
-  name   = "${var.do_k8s_name}-atlantis-lb"
-  region = var.do_region
-  size = "lb-small"
-  algorithm = "round_robin"
-
-  forwarding_rule {
-    entry_port     = 80 
-    entry_protocol = "http"
-
-    target_port     = 80
-    target_protocol = "http"
-
-  }
-
-  lifecycle {
-      ignore_changes = [
-        forwarding_rule,
-    ]
-  }
-
+  vpc_uuid = digitalocean_vpc.k8s.id 
 }
