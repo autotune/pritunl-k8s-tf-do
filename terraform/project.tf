bb6e7c7822ce44a654b791735798d96b36737f0f
Brian Adams <service@contrasting.org>
2022-02-20 17:05:46 -0600

    fix: remove k8s resource from project

--- terraform/project.tf
+++ terraform/project.tf
@@ -2,8 +2,4 @@ resource "digitalocean_project" "atlantis" {
   name        = "atlantis"
   description = "Sets up atlantis on DigitalOcean"
   environment = "Production"
-
-  resources = [
-    digitalocean_kubernetes_cluster.k8s.urn[0]
-  ]
 }
