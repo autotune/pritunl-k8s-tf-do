02bee29c2e06d7924d2ad556810658b27c3e32e1
Brian Adams <service@contrasting.org>
2022-02-20 15:58:54 -0600

    fix: create new backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     endpoint                    = "nyc3.digitaloceanspaces.com"
-    key                         = "pritunl.tfstate"
+    key                         = "atlantis.tfstate"
     bucket                      = "badams"
     region                      = "us-east-1"
     skip_credentials_validation = true
