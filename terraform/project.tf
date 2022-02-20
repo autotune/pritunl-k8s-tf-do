c5df2dfb20bd4204cb785a130eca9696a1c0afa4
Brian Adams <service@contrasting.org>
2022-02-20 16:34:59 -0600

    fix: use vpc in k8s

---
+++ terraform/project.tf
@@ -0,0 +1,9 @@
+resource "digitalocean_project" "atlantis" {
+  name        = "atlantis"
+  description = "Sets up atlantis on DigitalOcean"
+  environment = "Production"
+
+  resources = [
+    digitalocean_kubernetes_cluster.k8s.urn
+  ]
+}
