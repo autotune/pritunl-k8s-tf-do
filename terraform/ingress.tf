f8cf111d0841f65fd06fa6eaa6d42e4e478f0dba
Brian Adams <service@contrasting.org>
2022-02-22 08:46:09 -0600

    fix: change from ssl.com to zerossl

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -26,7 +26,7 @@ resource "kubernetes_ingress" "atlantis_cluster_ingress" {
     annotations = {
         "kubernetes.io/ingress.class" = "nginx"
         "ingress.kubernetes.io/rewrite-target" = "/"
-        "cert-manager.io/cluster-issuer" = "ssl-com"
+        "cert-manager.io/cluster-issuer" = "zerossl"
     }
   }
   spec {
