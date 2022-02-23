354fc77f1f2a4ca9c078d6268e6ea73524f39ad8
Brian Adams <service@contrasting.org>
2022-02-23 13:14:59 -0600

    fix: spelling

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -26,3 +26,15 @@ resource "digitalocean_record" "atlantis" {
     kubernetes_ingress.atlantis_cluster_ingress
   ]
 }
+
+resource "digitalocean_record" "auth" {
+  for_each = toset(var.domain_name)
+  domain = each.value
+  type   = "A"
+  ttl = 60
+  name   = "auth"
+  value  = digitalocean_loadbalancer.ingress_load_balancer.ip
+  depends_on = [
+    kubernetes_ingress.atlantis_cluster_ingress
+  ]
+}
