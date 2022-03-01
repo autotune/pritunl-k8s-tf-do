fab0721b0b50995ffa3b6467d26202811aa880a2
Brian Adams <service@contrasting.org>
2022-03-01 13:55:58 -0600

    fix: whitelist github ip ranges

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,5 @@
 locals {
   name      = "${var.do_k8s_name}"
-  gh_ips    =  for range in data.github_ip_ranges.default.hooks : cidrhost(range, 0)
+  gh_ips    =  for range in data.github_ip_ranges.default.hooks : 
+               cidrhost(range, 0)
 }
