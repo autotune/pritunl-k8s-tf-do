ec47e3a1f6449f8e6e27efd0fc6e6f2bf16270a9
Brian Adams <service@contrasting.org>
2022-03-03 17:34:20 -0600

    fix: template file doesn't change

--- terraform/atlantis/data.tf
+++ terraform/atlantis/data.tf
@@ -1,15 +1,15 @@
 data "template_file" "argocd" {
   template = "${path.module}/argocd/values.yaml.tpl"
   vars {
-        "argocd_server_host"          = var.argocd_server_host
-        "argocd_github_client_id"     = var.oauth_client_id
-        "argocd_github_client_secret" = var.oauth_client_secret
+        argocd_server_host          = var.argocd_server_host
+        argocd_github_client_id     = var.oauth_client_id
+        argocd_github_client_secret = var.oauth_client_secret
 
-        "argocd_ingress_enabled"                 = var.argocd_ingress_enabled
-        "argocd_ingress_tls_acme_enabled"        = var.argocd_ingress_tls_acme_enabled
-        "argocd_ingress_ssl_passthrough_enabled" = var.argocd_ingress_ssl_passthrough_enabled
-        "argocd_ingress_class"                   = var.argocd_ingress_class
-        "argocd_ingress_tls_secret_name"         = var.argocd_ingress_tls_secret_name
+        argocd_ingress_enabled                 = var.argocd_ingress_enabled
+        argocd_ingress_tls_acme_enabled        = var.argocd_ingress_tls_acme_enabled
+        argocd_ingress_ssl_passthrough_enabled = var.argocd_ingress_ssl_passthrough_enabled
+        argocd_ingress_class                   = var.argocd_ingress_class
+        argocd_ingress_tls_secret_name         = var.argocd_ingress_tls_secret_name
   }
 }
 
