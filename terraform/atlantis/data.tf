e102a8b2a4c478b72b0ca34e4110c9c6ce0b5088
Brian Adams <service@contrasting.org>
2022-03-03 13:32:04 -0600

    fix: rename k8s.tf to data.tf

--- terraform/atlantis/data.tf
+++ terraform/atlantis/data.tf
@@ -4,19 +4,19 @@ data "digitalocean_kubernetes_cluster" "k8s" {
 
 provider "kubernetes" {
   host                   = data.digitalocean_kubernetes_cluster.k8s.endpoint
-  token                  = data.digitalocean_kubernetes_cluster.k8s.kube_config.token
+  token                  = data.digitalocean_kubernetes_cluster.k8s.kube_config[0].token
   cluster_ca_certificate = base64decode(
-    data.digitalocean_kubernetes_cluster.k8s.kube_config.cluster_ca_certificate
+    data.digitalocean_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
   )
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.digitalocean_kubernetes_cluster.k8s.endpoint
-    token                  = data.digitalocean_kubernetes_cluster.k8s.kube_config.token
+    token                  = data.digitalocean_kubernetes_cluster.k8s.kube_config[0].token
 
     cluster_ca_certificate = base64decode(
-    data.digitalocean_kubernetes_cluster.k8s.kube_config.cluster_ca_certificate
+    data.digitalocean_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
     )
   }
 }
