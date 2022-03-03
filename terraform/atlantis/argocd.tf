18f0ee1ed78783d1b1c38e1c0931d47cbb886bda
Brian Adams <service@contrasting.org>
2022-03-03 17:31:27 -0600

    fix: template file doesn't change

--- terraform/atlantis/argocd.tf
+++ terraform/atlantis/argocd.tf
@@ -17,18 +17,7 @@ resource "helm_release" "argocd" {
 
   values = [
     templatefile(
-      "${path.module}/argocd/values.yaml.tpl",
-      {
-        "argocd_server_host"          = var.argocd_server_host
-        "argocd_github_client_id"     = var.oauth_client_id
-        "argocd_github_client_secret" = var.oauth_client_secret
-
-        "argocd_ingress_enabled"                 = var.argocd_ingress_enabled
-        "argocd_ingress_tls_acme_enabled"        = var.argocd_ingress_tls_acme_enabled
-        "argocd_ingress_ssl_passthrough_enabled" = var.argocd_ingress_ssl_passthrough_enabled
-        "argocd_ingress_class"                   = var.argocd_ingress_class
-        "argocd_ingress_tls_secret_name"         = var.argocd_ingress_tls_secret_name
-      }
+        data.template_file.argocd  
     )
   ]
 }
--- terraform/atlantis/data.tf
+++ terraform/atlantis/data.tf
@@ -1,3 +1,18 @@
+data "template_file" "argocd" {
+  template = "${path.module}/argocd/values.yaml.tpl"
+  vars {
+        "argocd_server_host"          = var.argocd_server_host
+        "argocd_github_client_id"     = var.oauth_client_id
+        "argocd_github_client_secret" = var.oauth_client_secret
+
+        "argocd_ingress_enabled"                 = var.argocd_ingress_enabled
+        "argocd_ingress_tls_acme_enabled"        = var.argocd_ingress_tls_acme_enabled
+        "argocd_ingress_ssl_passthrough_enabled" = var.argocd_ingress_ssl_passthrough_enabled
+        "argocd_ingress_class"                   = var.argocd_ingress_class
+        "argocd_ingress_tls_secret_name"         = var.argocd_ingress_tls_secret_name
+  }
+}
+
 data "digitalocean_kubernetes_cluster" "k8s" {
   name = local.name
 }
