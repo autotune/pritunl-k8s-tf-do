9367e421ad6eb122bc5f79b5c4a54b0c002de050
Brian Adams <service@contrasting.org>
2022-03-06 17:16:38 -0600

    fix: remove argocd create mongodb

---
+++ terraform/atlantis/mongodb.tf
@@ -0,0 +1,19 @@
+# based off of https://github.com/anarkioteam/terraform-helm-argocd
+
+resource "kubernetes_namespace" "mongodb" {
+  metadata {
+    name = "mondodb"  
+  }
+}
+
+resource "helm_release" "mongodb" {
+  depends_on = [kubernetes_namespace.mongodb]
+
+  name       = "mongodb"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "mongodb"
+  namespace  = "mongodb" 
+  version    = var.mongodb_version
+
+  # values   = [ sensitive(data.template_file.mongodb.rendered) ] 
+}
