5f4dc214d938338f3e24d241158ef1f83db50464
Brian Adams <service@contrasting.org>
2022-03-06 22:21:41 -0600

    add package registry secret

--- terraform/atlantis/locals.tf
+++ terraform/atlantis/locals.tf
@@ -1,5 +1,5 @@
 locals {
   name                  = "${var.do_k8s_name}"
   docker_secret         = "${var.gh_username}:${var.package_registry_pat}" 
-  docker_secret_encoded = base64encode(docker_secret)
+  docker_secret_encoded = base64encode(local.docker_secret)
 }
