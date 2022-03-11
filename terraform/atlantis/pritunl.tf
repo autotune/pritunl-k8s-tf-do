0a3dc5c9fef268da44012cb25a9a41a450f60727
Brian Adams <service@contrasting.org>
2022-03-11 17:07:39 -0600

    feat: add dockerconfig

--- terraform/atlantis/pritunl.tf
+++ terraform/atlantis/pritunl.tf
@@ -13,7 +13,7 @@ resource "helm_release" "pritunl" {
   repository = "./helm_charts"
   chart      = "pritunl"
   namespace  = "pritunl"
-  version    = "0.0.1" 
+  version    = "0.0.2" 
 
   values = [ file("${path.module}/pritunl/values.yaml.tpl") ]
 }
