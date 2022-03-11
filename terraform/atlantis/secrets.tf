ee426079e103d27f36440c1b7c7ed155f0a4e132
Brian Adams <service@contrasting.org>
2022-03-11 16:45:54 -0600

    feat: add dockerconfig

---
+++ terraform/atlantis/secrets.tf
@@ -0,0 +1,13 @@
+resource "kubernetes_secret" "docker_login_secret" {
+  for_each = toset(var.domain_name)
+  metadata {
+    name      = "${replace(each.key, ".", "-")}-docker-login"
+    namespace = "oauth-proxy"
+  }
+
+  data = {
+      .dockerconfigjson = base64encode(sensitive(data.template_file.docker_registry.rendered))
+  }
+
+  type = "kubernetes.io/opaque"
+}
