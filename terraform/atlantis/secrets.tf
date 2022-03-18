685a248ebbc138787a214dc45335c09e93b26631
Brian Adams <service@contrasting.org>
2022-03-17 20:17:13 -0500

    fix: secrets

--- terraform/atlantis/secrets.tf
+++ terraform/atlantis/secrets.tf
@@ -5,7 +5,7 @@ resource "kubernetes_secret" "docker_login_secret" {
   }
 
   data = {
-      ".dockerconfigjson": "${data.template_file.docker_registry.rendered}"
+      ".dockerconfigjson": "${jsonencode(data.template_file.docker_registry.rendered)}"
   }
 
   type = "kubernetes.io/dockerconfigjson" 
