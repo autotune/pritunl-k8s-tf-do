b19ed0f962dbb6509d9d57fe14afb70102910c89
Brian Adams <service@contrasting.org>
2022-03-27 15:47:02 -0500

    fix: add bitnami-common

--- terraform/atlantis/mongodb.tf
+++ terraform/atlantis/mongodb.tf
@@ -4,8 +4,19 @@ resource "kubernetes_namespace" "mongodb" {
   }
 }
 
+resource "helm_release" "common" {
+
+  name       = "bitnami-common"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "bitnami-common"
+  namespace  = "pritunl" 
+  version    = "1.x.x" 
+
+  values   = [ sensitive(data.template_file.mongodb.rendered) ] 
+}
+
 resource "helm_release" "mongodb" {
-  depends_on = [kubernetes_namespace.mongodb]
+  depends_on = [helm_release.common]
 
   name       = "pritunl-mongodb"
   repository = "https://charts.bitnami.com/bitnami"
