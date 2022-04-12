2c4f595b1cbbb13b2de56277812746bbb6ab1944
Brian Adams <service@contrasting.org>
2022-04-11 22:06:51 -0500

    fix: added mongodb root password var

--- terraform/cluster_resources.tf
+++ terraform/cluster_resources.tf
@@ -73,7 +73,7 @@ resource "kubernetes_deployment" "atlantis_deployment" {
           }
 
           env {
-            name  = "MONGODB_ROOT_PASSWORD"
+            name  = "TF_VAR_MONGODB_ROOT_PASSWORD"
             value = var.mongodb_root_password
           }
 
