2b5f0cf0812c92a45bbaab0cf709e94545312ebb
Brian Adams <service@contrasting.org>
2022-04-18 10:01:01 -0500

    fix: added mongodb root password

--- terraform/atlantis/pritunl.tf
+++ terraform/atlantis/pritunl.tf
@@ -13,7 +13,7 @@ resource "helm_release" "pritunl" {
   repository = "./helm_charts"
   chart      = "pritunl"
   namespace  = "pritunl"
-  version    = "0.0.4" 
+  version    = "0.0.6" 
 
   values = [ data.template_file.pritunl.rendered ]
 }
