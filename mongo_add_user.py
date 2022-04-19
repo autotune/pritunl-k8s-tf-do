f44ad73faa0178814baaa0e918e2b42e81dd23bf
Brian Adams <service@contrasting.org>
2022-04-18 22:02:01 -0500

    feat: mongodb_add_user.py

--- mongo_add_user.py
+++ mongo_add_user.py
@@ -1,14 +1,22 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
-from os import environ 
+from os import environ
 from pymongo import MongoClient
 
-mongodb_host          = environ.get('MONGO_HOST')
-mongodb_port          = environ.get('PRITUNL_MONGODB_SERVICE_PORT_MONGODB')
+# mongodb_host          = environ.get('MONGO_HOST')
+# mongodb_port          = environ.get('PRITUNL_MONGODB_SERVICE_PORT_MONGODB')
 mongodb_root_password = environ.get('MONGODB_ROOT_PASSWORD')
 
-client = MongoClient("pritunl-mongo:2107",
+client = MongoClient("pritunl-mongodb:27017",
                       username='root',
                       password='{}'.format(mongodb_root_password))
+                      
+pritunldb = client["pritunl"]
+dblist = pritunldb.list_database_names()
 
-client.testdb.add_user('newTestUser', 'Test123', roles=[{'role':'readWrite','db':'testdb'}])
+if "pritunl" not in dblist:
+    pritunldb = client["pritunl"]
+    pritunldb.command("createUser", "admin", pwd="{}".format(mongodb_root_password), roles=["readWrite"])
+else: 
+    print("DB, and likely user, already exist") 
+
