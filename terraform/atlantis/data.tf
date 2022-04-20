77d5eb5e3f7ec06128bd559941785f70e4e414db
Brian Adams <service@contrasting.org>
2022-04-19 20:06:12 -0500

    fix: data.tf

--- terraform/atlantis/data.tf
+++ terraform/atlantis/data.tf
@@ -1,5 +1,27 @@
 data "template_file" "pritunl" {
-  template = "${path.module}/pritunl/values.yaml.tpl"
+  template = file("${path.module}/pritunl/values.yaml.tpl")
+  vars = {
+    DOMAIN_NAME     = replace(var.domain_name, ".", "-")
+    DOCKER_REPO     = "${var.gh_username}/pritunl-k8s-tf-do"
+    DOCKER_TAG      = "pritunl:dc7700e4"
+    DOCKER_REGISTRY = "ghcr.io"
+  }
+}
+
+data "template_file" "docker_registry" {
+  template = "${path.module}/docker/values.yaml.tpl"
+
+  vars ={ 
+    docker_secret_encoded = local.docker_secret_encoded
+  }
+}
+
+data "template_file" "mongodb" {
+  template = file("${path.module}/mongodb/values.yaml.tpl")
+
+  vars ={ 
+    ROOTPASSWORD = var.mongodb_root_password
+  }
 }
 
 data "digitalocean_kubernetes_cluster" "k8s" {
