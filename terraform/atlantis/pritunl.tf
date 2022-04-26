1fac71669d3186a19c0378190c76c68187755de1
Brian Adams <service@contrasting.org>
2022-04-25 20:17:09 -0500

    fix: added vpn ingress

--- terraform/atlantis/pritunl.tf
+++ terraform/atlantis/pritunl.tf
@@ -13,7 +13,7 @@ resource "helm_release" "pritunl" {
   repository = "./helm_charts"
   chart      = "pritunl"
   namespace  = "pritunl"
-  version    = "0.1.3" 
+  version    = "0.1.5" 
 
   values = [ data.template_file.pritunl.rendered ]
 }
